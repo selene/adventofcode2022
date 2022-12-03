7d47cf28a3e25c22b802cf0de61a65f96e322cc0
HOPPER\Selene <me@selenetan.com>
2022-12-02 23:19:06 -0800

    Day 3 Part 1 solution. Now with file reading!!!

---
+++ src/03_advent.hs
@@ -0,0 +1,38 @@
+import System.Environment
+import Data.Text ()
+import qualified Data.Text.IO as TIO
+import qualified Data.Set as Set
+import Data.List
+import Data.Maybe
+
+testInput = "vJrwpWtwJgWrhcsFMMfFFhFp\n\
+  \jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\n\
+  \PmmdzqPrVvPwwTWBwg\n\
+  \wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\n\
+  \ttgJtRGJQctTZtZT\n\
+  \CrZsJsPPZsGzwwsLwLmpwMDw\n"
+
+
+
+itemChars = ['a'..'z'] ++ ['A'..'Z']
+
+rucksackToCompartments :: String -> (Set.Set Char, Set.Set Char)
+rucksackToCompartments r = (Set.fromList comp1, Set.fromList comp2)
+  where len = length r
+        (comp1, comp2) = splitAt (len `div` 2) r
+
+findDuplicate :: (Set.Set Char, Set.Set Char) -> Char
+findDuplicate (comp1, comp2) = head (Set.toList (Set.intersection comp1 comp2))
+
+itemPriority :: Char -> Int
+itemPriority x = 1 + (fromJust (elemIndex x itemChars))
+
+rucksackDuplicatePriority :: String -> Int
+rucksackDuplicatePriority rucksack = itemPriority (findDuplicate (rucksackToCompartments rucksack))
+
+solve01 :: String -> Int
+solve01 input = sum (map rucksackDuplicatePriority (lines input))
+
+main = do
+  input <- readFile "03input.txt"
+  putStrLn $ show (solve01 input)
