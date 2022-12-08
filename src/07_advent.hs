61be1752f9fa5448678e58febab11a59cca85dfc
HOPPER\Selene <me@selenetan.com>
2022-12-07 22:22:56 -0800

    Function to get the sum of small dirs.

--- src/07_advent.hs
+++ src/07_advent.hs
@@ -46,20 +46,23 @@ getDirSizes sizesByDir dir@(Dir parent name subdirs files) = Map.insert (dir) (d
   where fileSizes = (sum (map getFileSize files))
         updatedSizesByDir = Map.unions ([sizesByDir] ++ (map (getDirSizes Map.empty) subdirs))
         dirSizes = sum [fromJust (Map.lookup subdir updatedSizesByDir) | subdir <- subdirs]
--- getDirSizes (sizesByDir, dir@(Dir parent name (subdir:xs) files)) = Map.insert (dir) (dirSize + fileSizes) sizesByDir
---   where fileSizes = (sum (map getFileSize files))
---         updatedSizesByDir = getDirSizes(sizesByDir, subdir)
---         dirSize = Map.lookup subdir updatedSizesByDir
--- -- getDirSizes (sizesByDir, dir@(Dir parent name subdirs files)) = Map.insert (dir)  (fileSize + dirSize) sizesByDir
---   where
---     fileSize = sum (map getFileSize files)
---     -- dirMap = getDirSizes (sizesByDir, ) -- Need to run getDirSizes on each subdir, and combine the resulting map with the map from the previous call
---     -- probably something like reduce or foldl?
---     dirMap = foldl (getDirSizes) sizesByDir subdirs
---     dirSize = sum [Map.lookup d dirMap | d <- subdirs]
 
 
 testEmptyDir = Dir "/" Nothing [] []
 testFilesOnlyDir = Dir "/" Nothing [] [File "a" 11, File "b" 22]
 testDirsOnlyDir = Dir "/" Nothing [testFilesOnlyDir] []
 testDir = Dir "/" Nothing [Dir "stuff" Nothing [Dir "empty" Nothing [] []] [File "a" 11, File "d" 44]] [File "b" 22, File "c" 33]
+testBigDir = Dir "/" Nothing [Dir "stuff" Nothing [Dir "empty" Nothing [] []] [File "a" 111, File "d" 44]] [File "b" 222333, File "c" 33]
+
+
+addIfSmall :: Integer -> Integer -> Integer
+addIfSmall a b
+  | b <= 100000 = a + b
+  | otherwise = a
+
+sumSmallDirs :: Map.Map Dir Integer -> Integer
+sumSmallDirs dirMap = Map.foldl addIfSmall 0 dirMap
+
+
+test01Empty = sumSmallDirs (getDirSizes Map.empty testEmptyDir)
+test01BigDir = sumSmallDirs (getDirSizes Map.empty testBigDir)
