97e86e7e64d415e336f0f2834dceb3ec89979714
HOPPER\Selene <me@selenetan.com>
2022-12-10 22:30:27 -0800

    Solved Day 11 part 1

--- src/11_advent.py
+++ src/11_advent.py
@@ -27,17 +27,20 @@ class Monkey():
         self.test = test
         self.true_target = true_target
         self.false_target = false_target
+        
+        self.inspections = 0
 
     
     def __str__(self):
-        return f'Monkey[{self.id}] items={self.items} op={self.operation} test={self.test} targets={self.true_target},{self.false_target}'
+        return f'Monkey[{self.id}] items={self.items} op={self.operation} test={self.test} targets={self.true_target.id},{self.false_target.id}'
 
     def inspect(self):
-        self.items = [self.operation(item) for item in items]
+        self.items = [self.operation(item) for item in self.items]
+        self.inspections += len(self.items)
         return self.items
     
     def relax(self):    
-        self.items = [math.floor(item / 3.0) for item in items]
+        self.items = [math.floor(item / 3.0) for item in self.items]
         return self.items
     
     def get_throw_target(self, item):
@@ -46,11 +49,19 @@ class Monkey():
         else:
             return self.false_target
     
-    def throw_targets(self):
-        return [
-            self.get_throw_target(item)
-            for item in items
-        ]
+    def throw_items(self):
+        for item in self.items:
+            target = self.get_throw_target(item)
+            print(f'     Throwing {item} to monkey {target.id}')
+            target.catch(item)
+            print(f'       Monkey#{target.id} items now = {target.items}')
+        
+        self.items.clear()
+            
+    
+    def catch(self, item):
+        self.items.append(item)
+        
 
 
 def initialize(use_input=False, value=None):
@@ -62,32 +73,32 @@ def initialize(use_input=False, value=None):
         input_lines = value.split('\n')
     else:
         input_lines = """Monkey 0:
-  Starting items: 79, 98
-  Operation: new = old * 19
-  Test: divisible by 23
-    If true: throw to monkey 2
-    If false: throw to monkey 3
+Starting items: 79, 98
+Operation: new = old * 19
+Test: divisible by 23
+If true: throw to monkey 2
+If false: throw to monkey 3
 
 Monkey 1:
-  Starting items: 54, 65, 75, 74
-  Operation: new = old + 6
-  Test: divisible by 19
-    If true: throw to monkey 2
-    If false: throw to monkey 0
+Starting items: 54, 65, 75, 74
+Operation: new = old + 6
+Test: divisible by 19
+If true: throw to monkey 2
+If false: throw to monkey 0
 
 Monkey 2:
-  Starting items: 79, 60, 97
-  Operation: new = old * old
-  Test: divisible by 13
-    If true: throw to monkey 1
-    If false: throw to monkey 3
+Starting items: 79, 60, 97
+Operation: new = old * old
+Test: divisible by 13
+If true: throw to monkey 1
+If false: throw to monkey 3
 
 Monkey 3:
-  Starting items: 74
-  Operation: new = old + 3
-  Test: divisible by 17
-    If true: throw to monkey 0
-    If false: throw to monkey 1""".split('\n')
+Starting items: 74
+Operation: new = old + 3
+Test: divisible by 17
+If true: throw to monkey 0
+If false: throw to monkey 1""".split('\n')
     
     return input_lines
 
@@ -111,7 +122,7 @@ def create_operation(operation, value):
 
 
 def create_test(operand):
-    return lambda x: x % operand
+    return lambda x: x % operand == 0
 
 
 def parse_lines(lines):
@@ -133,36 +144,66 @@ def parse_lines(lines):
             print(f'Monkey[{current_monkey.id}].items = {current_monkey.items}')
             continue
         
-        operation = parse('  Operation: new = old {op} {val}', line)
+        operation = parse('Operation: new = old {op} {val}', line)
         if operation:
             current_monkey.operation = create_operation(operation['op'], operation['val'])
-            print(f'Monkey[{current_monkey.id}].')
+            print(f'Monkey[{current_monkey.id}].op = {current_monkey.operation}')
             continue
         
-        test = parse('  Test: divisible by {num:d}', line)
+        test = parse('Test: divisible by {num:d}', line)
         if test:
             current_monkey.test = create_test(test['num'])
+            
+            print(f'Monkey[{current_monkey.id}].test = {current_monkey.test}')
             continue
         
-        true_target = parse('    If true: throw to monkey {id:d}', line)
+        true_target = parse('If true: throw to monkey {id:d}', line)
         if true_target:
             current_monkey.true_target = true_target['id']
+            print(f'Monkey[{current_monkey.id}].true_target = {current_monkey.true_target}')
             continue
             
-        false_target = parse('    If false: throw to monkey {id:d}', line)
+        false_target = parse('If false: throw to monkey {id:d}', line)
         if false_target:
             current_monkey.false_target = false_target['id']
+            print(f'Monkey[{current_monkey.id}].false_target = {current_monkey.false_target}')
             continue
     
+    print(f'Parsed {len(monkeys)} monkeys')
+    
+    for monkey in monkeys:
+        monkey.true_target = monkeys[monkey.true_target]
+        monkey.false_target = monkeys[monkey.false_target]
+    
     return monkeys
+
+
+def calculate_monkey_business(monkeys):
+    inspections = sorted([m.inspections for m in monkeys], reverse=True)
+    
+    return inspections[0] * inspections[1]
+    
 
 
 def part1(use_input=False, value=None):
     monkeys = parse_lines(initialize(use_input, value))
     print('\n'.join([str(m) for m in monkeys]))
     
-
-
+    rounds = 20
+    for i in range(rounds):
+        print(f'\n~~~ Round {i} ~~~')
+        for m in monkeys:
+            print(f'  ** Monkey {m.id}')
+            print(f'     items = {m.items}')
+            m.inspect()
+            print(f'     after inspection = {m.items}')
+            m.relax()
+            print(f'     after relax = {m.items}')
+            m.throw_items()
+            
+    monkey_business = calculate_monkey_business(monkeys)
+    print(monkey_business)
+    return monkey_business
 
     
 def part2(use_input=False, value=None):
